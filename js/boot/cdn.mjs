5f6e3ce85fcd7f794f4066c894aed9ee384a6fc9
Jeff-Aporta <jeffaporta@gmail.com>
2026-06-17 10:04:08 -0500

    fix: Se actualizó el pin de front-shared a 9089a8e

--- js/boot/cdn.mjs
+++ js/boot/cdn.mjs
@@ -1,3 +1,3 @@
-export const PIN = "989de09";
+export const PIN = "9089a8e";
 export const CDN = `https://cdn.jsdelivr.net/gh/Jeff-Aporta/front-shared@${PIN}/cdn`;
 export const asset = (p) => `${CDN}/${p}?v=${PIN}`;
