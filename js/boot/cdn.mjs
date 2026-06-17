5c766f7b84f798255c6425e22f46323949e50480
Jeff-Aporta <jeffaporta@gmail.com>
2026-06-17 10:25:36 -0500

    chore: Se actualizó el pin de front-shared a 0f362af

--- js/boot/cdn.mjs
+++ js/boot/cdn.mjs
@@ -1,3 +1,3 @@
-export const PIN = "386911a";
+export const PIN = "0f362af";
 export const CDN = `https://cdn.jsdelivr.net/gh/Jeff-Aporta/front-shared@${PIN}/cdn`;
 export const asset = (p) => `${CDN}/${p}?v=${PIN}`;
